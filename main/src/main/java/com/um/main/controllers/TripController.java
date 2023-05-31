0d3ded2d23f86980569104101d37eab59771825e
DouglasArenas <douglasarenas71@gmail.com>
2023-05-30 21:22:58 -0300

    #1 adding controllers

---
+++ main/src/main/java/com/um/main/controllers/TripController.java
@@ -0,0 +1,50 @@
+package com.um.main.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/trip")
+public class TripController {
+    @Autowired
+    private TripService tripService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Trip> getTrip(@PathVariable Long id) {
+        Trip trip = tripService.getTrip(id);
+        return new ResponseEntity<>(trip, HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<Trip> addTrip(@RequestBody Trip trip) {
+        Trip newTrip = tripService.addTrip(trip);
+        return new ResponseEntity<>(trip, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Trip> updateTrip(@PathVariable Long id, @RequestBody Trip trip) {
+        trip.setId(id);
+        Trip updateTrip = tripService.updateTrip(trip);
+        return new ResponseEntity<>(updateTrip, HttpStatus.OK);
+    }
+ 
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Trip> deleteTrip(@PathVariable Long id) {
+        tripService.deleteTrip(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT):
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<List<Trip>> getAllTrips() {
+        List<Trip> trips = tripService.getAllTrips();
+        return new ResponseEntity<>(trips, HttpStatus.OK);
+    }
+}
