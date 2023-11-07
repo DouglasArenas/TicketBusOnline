9cab229e79f0b4d1ce126a4ece42badd443e3012
Douglas <douglasarenas71@gmail.com>
2023-11-07 01:47:07 -0300

    Refactor CityRepository and TripRepository. Refs #5

--- main/src/main/java/com/um/main/repositories/CityRepository.java
+++ main/src/main/java/com/um/main/repositories/CityRepository.java
@@ -2,7 +2,6 @@ package com.um.main.repositories;
 
 import com.um.main.models.City;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 public interface CityRepository extends JpaRepository<City, Long> {
     City findByName(String name);
--- main/src/main/java/com/um/main/repositories/TripRepository.java
+++ main/src/main/java/com/um/main/repositories/TripRepository.java
@@ -1,15 +1,18 @@
 package com.um.main.repositories;
 
+import com.um.main.models.City;
 import com.um.main.models.Trip;
 
-import java.sql.Date;
+import java.sql.Timestamp;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 
 public interface TripRepository extends JpaRepository<Trip, Long> {
-    List<Trip> findByDepartureTime(Date departureTime);
+    List<Trip> findByDepartureTimeGreaterThanEqual(Timestamp departure_time);
+    @Query("SELECT t FROM Trip t WHERE t.origin = :origin AND t.destination = :destination AND t.departureTime >= :departureTimeStart AND t.departureTime < :departureTimeEnd")
+List<Trip> findByOriginAndDestinationAndDate(@Param("origin") City origin, @Param("destination") City destination, @Param("departureTimeStart") Timestamp departureTimeStart, @Param("departureTimeEnd") Timestamp departureTimeEnd);
 }
-
-
